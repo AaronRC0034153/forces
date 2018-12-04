f6bf89cbbf9cb53e0490a98e7fe4e49f325e392d
Aaron Cheung <rc0034153@student.riversidecollege.ac.uk>
2018-12-04 10:35:54 +0000

    All sin cos and tan complete and working

--- forces/Form1.cs
+++ forces/Form1.cs
@@ -16,5 +16,62 @@ namespace forces
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a1 = double.Parse(box1.Text);
+                double ans1 = sin(a1);
+                label1.Text = ans1.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+        }
+        private double sin(double a1)
+        {
+            double rad1 = a1 / 180.0 * Math.PI;
+            return Math.Sin(rad1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a2 = double.Parse(box2.Text);
+                double ans2 = cos(a2);
+                label2.Text = ans2.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+        }
+        private double cos(double a2)
+        {
+            double rad2 = a2 / 180.0 * Math.PI;
+            return Math.Cos(rad2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a3 = double.Parse(box3.Text);
+                double ans3 = tan(a3);
+                label3.Text = ans3.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+        }
+        private double tan(double a3)
+        {
+            double rad3 = a3 / 180.0 * Math.PI;
+            return Math.Tan(rad3);
+        }
     }
 }
