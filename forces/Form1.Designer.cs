5b4bb72d76cd9930b362f02add84bf8d8a6d51d6
Aaron Cheung <rc0034153@student.riversidecollege.ac.uk>
2018-12-04 10:51:40 +0000

    inverses working

--- forces/Form1.Designer.cs
+++ forces/Form1.Designer.cs
@@ -37,6 +37,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // box2
@@ -117,11 +120,44 @@
             this.label3.TabIndex = 9;
             this.label3.Text = "label3";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(235, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Inverse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(235, 219);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Inverse";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(235, 301);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Inverse";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -149,6 +185,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- forces/Form1.cs
+++ forces/Form1.cs
@@ -73,5 +73,59 @@ namespace forces
             double rad3 = a3 / 180.0 * Math.PI;
             return Math.Tan(rad3);
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double n1 = double.Parse(box1.Text);
+                double ans4 = asin(n1);
+                label1.Text = ans4.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+        }
+        private double asin(double n1)
+        {
+            return Math.Asin(n1) * 180.0 / Math.PI;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double n2 = double.Parse(box2.Text);
+                double ans5 = acos(n2);
+                label2.Text = ans5.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+        }
+        private double acos(double n2)
+        {
+            return Math.Acos(n2)*180.0/Math.PI;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double n3 = double.Parse(box3.Text);
+                double ans6 = atan(n3);
+                label3.Text = ans6.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+        }
+        private double atan(double n3)
+        {
+            return Math.Atan(n3) * 180.0 / Math.PI;
+        }
     }
 }
