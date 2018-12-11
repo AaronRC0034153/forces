9dc39007772f046d95bd59167632ed115889d8bc
Aaron Cheung <rc0034153@student.riversidecollege.ac.uk>
2018-12-11 09:51:51 +0000

    picture in

--- forces/Form1.Designer.cs
+++ forces/Form1.Designer.cs
@@ -31,72 +31,38 @@
             this.box2 = new System.Windows.Forms.TextBox();
             this.box3 = new System.Windows.Forms.TextBox();
             this.box1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // box2
             // 
-            this.box2.Location = new System.Drawing.Point(383, 219);
+            this.box2.Location = new System.Drawing.Point(591, 338);
             this.box2.Name = "box2";
             this.box2.Size = new System.Drawing.Size(100, 20);
             this.box2.TabIndex = 1;
             // 
             // box3
             // 
-            this.box3.Location = new System.Drawing.Point(383, 304);
+            this.box3.Location = new System.Drawing.Point(591, 423);
             this.box3.Name = "box3";
             this.box3.Size = new System.Drawing.Size(100, 20);
             this.box3.TabIndex = 2;
             // 
             // box1
             // 
-            this.box1.Location = new System.Drawing.Point(383, 130);
+            this.box1.Location = new System.Drawing.Point(591, 249);
             this.box1.Name = "box1";
             this.box1.Size = new System.Drawing.Size(100, 20);
             this.box1.TabIndex = 3;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(134, 127);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Sin of";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(134, 219);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Cos of";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(134, 301);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Tan of";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(544, 133);
+            this.label1.Location = new System.Drawing.Point(752, 252);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 7;
@@ -105,7 +71,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(544, 229);
+            this.label2.Location = new System.Drawing.Point(752, 348);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 8;
@@ -114,61 +80,37 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(544, 311);
+            this.label3.Location = new System.Drawing.Point(752, 430);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "label3";
             // 
-            // button4
+            // pictureBox1
             // 
-            this.button4.Location = new System.Drawing.Point(235, 127);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "Inverse";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(235, 219);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 11;
-            this.button5.Text = "Inverse";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(235, 301);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 12;
-            this.button6.Text = "Inverse";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.pictureBox1.Image = global::forces.Properties.Resources.arrowting;
+            this.pictureBox1.Location = new System.Drawing.Point(123, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(438, 299);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.box1);
             this.Controls.Add(this.box3);
             this.Controls.Add(this.box2);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,15 +121,10 @@
         private System.Windows.Forms.TextBox box2;
         private System.Windows.Forms.TextBox box3;
         private System.Windows.Forms.TextBox box1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- forces/Form1.cs
+++ forces/Form1.cs
@@ -36,93 +36,33 @@ namespace forces
             return Math.Sin(rad1);
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                double a2 = double.Parse(box2.Text);
-                double ans2 = cos(a2);
-                label2.Text = ans2.ToString();
-            }
-            catch
-            {
-                MessageBox.Show("Enter a number");
-            }
-        }
+       
         private double cos(double a2)
         {
             double rad2 = a2 / 180.0 * Math.PI;
             return Math.Cos(rad2);
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                double a3 = double.Parse(box3.Text);
-                double ans3 = tan(a3);
-                label3.Text = ans3.ToString();
-            }
-            catch
-            {
-                MessageBox.Show("Enter a number");
-            }
-        }
+        
         private double tan(double a3)
         {
             double rad3 = a3 / 180.0 * Math.PI;
             return Math.Tan(rad3);
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                double n1 = double.Parse(box1.Text);
-                double ans4 = asin(n1);
-                label1.Text = ans4.ToString();
-            }
-            catch
-            {
-                MessageBox.Show("Enter a number");
-            }
-        }
+       
         private double asin(double n1)
         {
             return Math.Asin(n1) * 180.0 / Math.PI;
         }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                double n2 = double.Parse(box2.Text);
-                double ans5 = acos(n2);
-                label2.Text = ans5.ToString();
-            }
-            catch
-            {
-                MessageBox.Show("Enter a number");
-            }
-        }
+        
         private double acos(double n2)
         {
             return Math.Acos(n2)*180.0/Math.PI;
         }
 
-        private void button6_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                double n3 = double.Parse(box3.Text);
-                double ans6 = atan(n3);
-                label3.Text = ans6.ToString();
-            }
-            catch
-            {
-                MessageBox.Show("Enter a number");
-            }
-        }
+        
         private double atan(double n3)
         {
             return Math.Atan(n3) * 180.0 / Math.PI;
